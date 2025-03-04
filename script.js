5a1a6906001907129edf2302a2c1d524d4f80c20
Denis13111 <denis.kalinger@gmail.com>
2025-03-04 19:05:31 +0600

    hw4

--- script.js
+++ script.js
@@ -132,3 +132,49 @@
 //         alert ('Неверное значение')
 //         break;
 // }
+// Домашнее задание 4
+// Задание 1
+// for (let a = 0; a < 2; a++) {
+//     console.log ('Привет!');
+// }
+
+// Задание 2
+// for (let i= 1; i < 6; i++) {
+//     console.log (i);
+// }
+
+// Задание 3
+// for (let i = 7; i < 23; i++) {
+//     console.log (i);
+// }
+
+// Задание 4
+// const obj = {
+//     Коля: '200',
+//     Вася: '300',
+//     Петя: '400'
+// }
+// for (const key in obj) {
+//     console.log (`${key} - зарплата ${obj [key]} доларов`)   
+// }
+
+// Задание 5
+
+// let n =1000;
+// let num = 0;
+// while (n >= 50) {
+//     n/=2;
+//     console.log(n);
+//     num++;
+// }
+// console.log (`Количество циклов: ${num}`);
+
+// Задание 6
+
+// let friday = 5;
+
+// let dayMonth = 31;
+
+// for (let i = friday; i <= dayMonth; i += 7) {
+//     console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
+// }
